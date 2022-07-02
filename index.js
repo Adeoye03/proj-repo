63a10dc004e62aadc4b25a1378ae1e46e43712f6
Adeoye03 <106995709+Adeoye03@users.noreply.github.com>
2022-07-02 08:37:45 +0100

    Updated

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-jquery(function($) {
-    var path = Window.location.href;
-    $('ul a').each(function () {
-       if (this.href === path) {
-        $(this).addclass('active');
-       } 
-    });    
-});
+function setActive() {
+    linkObj = document.getElementById('premier-topnav').getElementsByTagName('a');
+    for(i=0;i<linkObj.length;i++){
+        if (document.location.pathname === linkObj[i].getAttribute('href')>=0) {
+          linkObj[i].classList.add("active")
+        }
+    }
+        
+    }
+    window.onload = setActive;
