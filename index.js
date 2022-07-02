075ab77aed73f2cc0cb482c739890d381c0a43e3
Adeoye03 <106995709+Adeoye03@users.noreply.github.com>
2022-07-02 10:44:54 +0100

    Edited

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ var nav = navContainer.getElementsByClassName("nav-item");
 for (var i = 0; i < nav.length; i++) {
   nav[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
-    if (current.length > 0) {
       current[0].className = current[0].className.replace("active", "");
-    }
     this.className += "active";
     
   });
